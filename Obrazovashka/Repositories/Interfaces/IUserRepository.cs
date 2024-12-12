39132bcc67ca7c27c7c3adf5cb328fc05219d1eb
Markus <barikal1603@gmail.com>
2024-12-12 07:05:09 +0300

    Дополнены интерфейсы репозиториев

--- Obrazovashka/Repositories/Interfaces/IUserRepository.cs
+++ Obrazovashka/Repositories/Interfaces/IUserRepository.cs
@@ -7,8 +7,8 @@ namespace Obrazovashka.Repositories.Interfaces
     public interface IUserRepository
     {
         Task AddUserAsync(User user);
-        Task<User> GetUserByIdAsync(int id);
         Task<User> GetUserByEmailAsync(string email);
+        Task<User> GetUserByIdAsync(int id);
         Task UpdateUserAsync(User user);
     }
 }
